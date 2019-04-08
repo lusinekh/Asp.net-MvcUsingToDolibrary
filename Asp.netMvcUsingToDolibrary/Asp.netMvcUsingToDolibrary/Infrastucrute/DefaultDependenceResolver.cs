0bd9a8fc517f0becfaea532931f2ae7aa21cd580
User <User@DESKTOP-I87ABNR>
2019-04-07 23:56:34 -0700

    AddDependence

---
+++ Asp.netMvcUsingToDolibrary/Asp.netMvcUsingToDolibrary/Infrastucrute/DefaultDependenceResolver.cs
@@ -0,0 +1,30 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asp.netMvcUsingToDolibrary.Infrastucrute
+{
+    public class DefaultDependenceResolver : IDependencyResolver
+    {
+        IKernel kernel;
+
+        public void AddBind()
+        {
+
+
+        }
+
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
